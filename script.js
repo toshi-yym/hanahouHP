ddd0cfcab865c309d7c2789bfe1b2a90d5e60129
toshi-yym <t.s.m0210@gmail.com>
2025-06-30 14:21:26 +0900

    メニュー写真スライダー＆ドリンク・フード一覧を追加

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
 // Initialize AOS Library for scroll animations
+// Goods Swiper init
+new Swiper('.goods-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  autoplay: { delay: 4000 },
+  pagination: {
+    el: '.goods-swiper .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.goods-swiper .swiper-button-next',
+    prevEl: '.goods-swiper .swiper-button-prev'
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    640: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+// Menu Swiper init
+new Swiper('.menu-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  autoplay: { delay: 4000 },
+  pagination: {
+    el: '.menu-swiper .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.menu-swiper .swiper-button-next',
+    prevEl: '.menu-swiper .swiper-button-prev'
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
+});
+
+// Blog Swiper init
+new Swiper('.blog-swiper',{slidesPerView:3,spaceBetween:16,loop:true,autoplay:{delay:4000},pagination:{el:'.blog-swiper .swiper-pagination',clickable:true},navigation:{nextEl:'.blog-swiper .swiper-button-next',prevEl:'.blog-swiper .swiper-button-prev'},breakpoints:{640:{slidesPerView:1},768:{slidesPerView:2},1024:{slidesPerView:3}}});
+
 AOS.init({
   duration: 800,
   once: true
